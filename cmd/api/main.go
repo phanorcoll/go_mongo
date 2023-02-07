199899bd91881489b657adf2879b406750cdae63
Phanor Coll <phanorcoll@gmai.com>
2023-02-07 11:45:34 -0400

    ℹ️ Connected to Mongodb
    Created the files needed to connect Mongodb, also created the
    docker-compose file to start Mongodb, using the varialbes from .env.
    
    Initialized the configuration struct in the main.go file, and passed it
    down to the connection function for Mongodb.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/phanorcoll/go_mongo/internal/api"
+import (
+	"github.com/phanorcoll/go_mongo/internal/api"
+	"github.com/phanorcoll/go_mongo/pkg/config"
+	"github.com/phanorcoll/go_mongo/pkg/data"
+)
 
 func main() {
+	cfg := config.New()
+	db := data.NewMongoConnection(cfg)
+	defer db.Disconnect()
 	application := api.New()
 	application.Start()
 }
---
+++ pkg/data/connection.go
@@ -0,0 +1,51 @@
+package data
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/phanorcoll/go_mongo/pkg/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type Connection struct {
+	Client *mongo.Client
+	ctx    context.Context
+}
+
+// NewMongoConnection function    Creates a new connection to Mongodb
+func NewMongoConnection(cfg *config.Settings) Connection {
+	uri := fmt.Sprintf("mongodb://%s/%s", cfg.DbHost, cfg.DbName)
+
+	credentials := options.Credential{
+		Username: cfg.DbUser,
+		Password: cfg.DbPass,
+	}
+
+	clientOpts := options.Client().ApplyURI(uri).SetAuth(credentials)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOpts)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to database.")
+
+	return Connection{
+		Client: client,
+		ctx:    ctx,
+	}
+}
+
+// Disconnect method    Disconnects from the Mongo instance
+func (c Connection) Disconnect() {
+	c.Client.Disconnect(c.ctx)
+}
